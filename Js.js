cfa04f42d74bad5a5fa79e8d0793b44b30190083
Prathamesh1402 <ppatrikar1111@gmail.com>
2024-07-16 14:18:59 +0530

    Chamges in code

--- Js.js
+++ Js.js
@@ -1,1 +1,10 @@
-console.log("Hello");
+const Profile={
+    Uname: "shradhakhapra",
+    posts: 195,
+    followers: BigInt("596000"),
+    following: 1,
+    isFollow: true,
+    about: "Entrepreneur"
+};
+
+console.log(Profile);
